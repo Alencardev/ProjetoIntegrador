5f7f9dab695e9a507ad20604b5ecaf91419f70e1
leona <leona@DESKTOP-5I2RH0E>
2021-10-12 17:12:37 -0300

    Started the connection with DB

---
+++ src/nextlevel/jdbc/ConnectionFactory.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nextlevel.jdbc;
+
+import java.sql.Connection;
+/**
+ *
+ * @author Leonardo Drews Montibeller at ldmontibeller@gmail.com
+ */
+public class ConnectionFactory {
+    public Connection getConnection(){
+        try {
+            
+        } catch (Exception e) {
+        }
+    }
+}
